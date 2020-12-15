1872dc5438b74c162c6fefbe5d2d478876bb8790
nopalhilmy <naufal.farras.hilmy@gmail.com>
2020-12-15 12:37:22 +0700

    first commit

---
+++ BookStore-API/Contracts/IloggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface IloggerService
+    {
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogError(string message);
+        void LogDebug(string message);
+    }
+}
---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BookStore_API.Contracts;
+
+namespace BookStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IloggerService _logger;
+
+        public HomeController(IloggerService logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get Values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed Home - Info");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get Value By ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Accessed Home Get By Id - Debug");
+            return "value";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("Accessed Home Post - Error");
+        }
+
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            _logger.LogWarn("Accessed Home Put - Warning");
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("Accessed Home Delete - Warning");
+        }
+    }
+}
